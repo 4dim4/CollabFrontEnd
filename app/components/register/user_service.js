bb52c69072f9790a506434abac6cbc3d3c6065aa
4dim4 <hrideshh@yahoo.co.in>
2016-11-01 02:05:03 +0530

    modified user service

--- app/components/register/user_service.js
+++ app/components/register/user_service.js
@@ -15,37 +15,43 @@
         service.Create = Create;
         service.Update = Update;
         service.Delete = Delete;
+        service.Authenticate=Authenticate;
  
         return service;
  
         function GetAll() {
-            return $http.get('http://localhost:8080/CollabFrontEnd/UserDetails').then(handleSuccess, handleError('Error getting all users'));
+            return $http.get('http://localhost:8081/CollabBackEnd/UserDetails/').then(handleSuccess, handleError('Error getting all users'));
         }
  
         function GetById(id) {
-            return $http.get('http://localhost:8080/CollabFrontEnd/UserDetails/' + id).then(handleSuccess, handleError('Error getting user by id'));
+            return $http.get('http://localhost:8081/CollabBackEnd/UserDetails/' + id).then(handleSuccess, handleError('Error getting user by id'));
         }
  
         function GetByUsername(username) {
-            return $http.get('http://localhost:8080/CollabFrontEnd/UserDetails/' + username).then(handleSuccess, handleError('Error getting user by username'));
+            return $http.get('http://localhost:8085/CollobrationPortal/customers/' + username).then(handleSuccess, handleError('Error getting user by username'));
         }
  
         function Create(user) {
-            return $http.post('http://localhost:8080/CollabFrontEnd/UserDetails', user).then(handleSuccess, handleError('Error creating user'));
+            return $http.post('http://localhost:8081/CollabBackEnd/UserDetails/', user).then(handleSuccess, handleError('Error creating user'));
+        }
+
+        function Authenticate(id,password) {
+            return $http.post('http://localhost:8081/CollabBackEnd/UserDetails/authenticate/', {id: id,password: password}).then(handleSuccess, handleError('Authentication failed'));
         }
  
         function Update(user) {
-            return $http.put('http://localhost:8080/CollabFrontEnd/UserDetails/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
+            return $http.put('http://localhost:8085/CollobrationPortal/customers/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
         }
  
         function Delete(id) {
-            return $http.delete('http://localhost:8080/CollabFrontEnd/UserDetails/' + id).then(handleSuccess, handleError('Error deleting user'));
+            return $http.delete('http://localhost:8085/CollobrationPortal/customers/' + id).then(handleSuccess, handleError('Error deleting user'));
         }
  
-        // private functions
+        // private functions 
  
-        function handleSuccess(res) {
-            return res.data;
+        function handleSuccess(response) {
+            console.log(response.data)
+            return response.data;
         }
  
         function handleError(error) {
